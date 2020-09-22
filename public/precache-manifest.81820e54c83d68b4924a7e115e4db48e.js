608a0a9637b8fc911b390ab104a1f39111f40152
Har-SHAW <8888harsha4444@gmail.com>
2020-09-22 17:32:15 +0530

    merging frontend build

---
+++ public/precache-manifest.81820e54c83d68b4924a7e115e4db48e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05479659bc943f1114949a3640665e70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6c11ebf5c2628719d96",
+    "url": "/static/css/main.e4782a8d.chunk.css"
+  },
+  {
+    "revision": "9ae3c5b15a56f2f779c8",
+    "url": "/static/js/2.07c9a2fc.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/static/js/2.07c9a2fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6c11ebf5c2628719d96",
+    "url": "/static/js/main.e55f9207.chunk.js"
+  },
+  {
+    "revision": "0082a517f26dec4b9b03",
+    "url": "/static/js/runtime-main.8c5c8497.js"
+  }
+]);
